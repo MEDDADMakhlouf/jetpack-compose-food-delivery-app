a8ee95284568a5abb328246e0f6d4d85ad945925
MEDDADMakhlouf <makhloufmeddad@gmail.com>
2025-07-07 06:53:02 -0400

    product preview section

---
+++ app/src/main/java/com/example/fooddelivery/ui/screen/productDetailsScreen.kt
@@ -0,0 +1,21 @@
+package com.example.fooddelivery.ui.screen
+
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.fooddelivery.ui.screen.component.ProductPreviewSection
+
+@Composable
+fun ProductDetailsScreen(
+    modifier: Modifier = Modifier
+) {
+    val scrollableState = rememberScrollState()
+    Column(
+        modifier = modifier.verticalScroll(scrollableState)
+    ) {
+        ProductPreviewSection()
+    }
+}
